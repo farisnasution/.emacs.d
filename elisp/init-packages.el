6eb299facd59b621ac10c1af515285db1018360f
Faris Nasution <nasution.faris@gmail.com>
2014-05-01 14:54:24 +0700

    (package-initialize) should come after adding external repository.

--- elisp/init-packages.el
+++ elisp/init-packages.el
@@ -1,10 +1,10 @@
 (require 'package)
 
-(package-initialize)
-
 (when (>= emacs-major-version 24)
   (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
-  (add-to-list 'package-archives '("melpa" . "http://melpa.milbox.net/packages/") t))
+  (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t))
+
+(package-initialize)
 
 (defun require-package (package-name)
   (when (not (package-installed-p package-name))
