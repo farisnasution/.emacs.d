3a0aa0bc5205f91c0168fffcf501a2984c6986fa
Faris Nasution <nasution.faris@gmail.com>
2014-05-01 14:45:03 +0700

    Fix typo.

--- elisp/init-packages.el
+++ elisp/init-packages.el
@@ -4,7 +4,7 @@
 
 (when (>= emacs-major-version 24)
   (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
-  (add-to-list 'package-archives '("melpa" . "http://melpa.milbox.net/packages/")) t)
+  (add-to-list 'package-archives '("melpa" . "http://melpa.milbox.net/packages/") t))
 
 (defun require-package (package-name)
   (when (not (package-installed-p package-name))
